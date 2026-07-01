b7d4e87d725cb2d1fb12892b74b5b2ce31941764
Sommith THOUMMALY <43395257+sommith@users.noreply.github.com>
2026-07-01 10:01:15 +0700

    Update app_dashboard.py

--- app_dashboard.py
+++ app_dashboard.py
@@ -131,6 +131,7 @@ air_label, air_text, air_color = get_status_info(pred_pm25, "air")
 soil_label, soil_text, soil_color = get_status_info(pred_soil_hum, "soil")
 
 # ສະແດງຜົນການທຳນາຍໃນກ່ອງສີ (Main Page)
+col_pred1, col_pred2 = st.columns(2)
 with col_pred1:
     st.subheader("🌤️ ຜົນການວິເຄາະຄຸນນະພາບອາກາດ (AI)")
     st.markdown(f'<div style="background-color:{air_color}; padding:20px; border-radius:10px; text-align:center;"><h2 style="color:white; margin:0;">{air_text}</h2><p style="color:white; font-size:20px; margin:10px 0 0 0;">ຄ່າ PM2.5: <b>{pred_pm25:.2f} µg/m³</b></p></div>', unsafe_allow_html=True)
